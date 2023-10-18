76cb78831f26917c4c88b6447402fabdbabfcb99
--help <guilhem.schlosser@gmail.com>
2023-10-18 15:12:37 +0200

    Reorganisation des dossiers

---
+++ Exercices/Seance5/premier_script.sh
@@ -0,0 +1,14 @@
+#!/user/bin/bash
+
+#On recupere les arguments donne dans le premier_script.sh
+YEAR=$1
+KEYWORD="$2"
+
+#On donne le chemin vers les fichiers
+FILE_PATH="./Fichiers/ann/201$YEAR/*/*.ann"
+
+#On declare la variable
+EXE=$(cat $FILE_PATH | egrep -w $KEYWORD | wc -l)
+
+#On affiche le resultat des variables
+echo "Pour l'annee 201$YEAR le mot '$KEYWORD' apparait : $EXE fois"
