140b82b6eeec653c9cd50f88537810622c0f7101
--help <guilhem.schlosser@gmail.com>
2023-10-21 19:12:40 +0200

    Update

--- Exercices/Seance4/premier_script.sh
+++ Exercices/Seance4/premier_script.sh
@@ -5,7 +5,7 @@ YEAR=$1
 KEYWORD="$2"
 
 #On donne le chemin vers les fichiers
-FILE_PATH="./Fichiers/ann/201$YEAR/*/*.ann"
+FILE_PATH="../../Fichiers/ann/201$YEAR/*/*.ann"
 
 #On declare la variable
 EXE=$(cat $FILE_PATH | egrep -w $KEYWORD | wc -l)
